9b34ce77510633f6dc55c5d367b530829f05945a
Santino <santinogomezrezza1214@gmail.com>
2023-10-17 22:08:00 -0300

    Nuevo

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const menuToggle = document.getElementById('menu-toggle');
-const nav = document.getElementById('nav');
-
-menuToggle.addEventListener('click', () => {
-  nav.classList.toggle('active');
-});
+document.addEventListener("DOMContentLoaded", function () {
+	const menuToggle = document.getElementById('menu-toggle');
+	const nav = document.getElementById('nav');
+  
+	menuToggle.addEventListener('click', () => {
+	  nav.classList.toggle('active');
+	});
+  });
 
 
 //----------------------------------------------------------------------------------------//
